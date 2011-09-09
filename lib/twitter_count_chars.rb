d4fd63852533c06af013c17ec8846bdb4cee8564
Shinya Kasatani <kasatani@gmail.com>
2011-09-09 17:16:02 +0900

    add implementation

---
+++ lib/twitter_count_chars.rb
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+require 'active_support'
+require 'active_support/core_ext/file'
+require 'tmpdir'
+require 'httpclient'
+require 'json'
+
+module TwitterCountChars
+  module_function
+
+  CONFIG_UPDATE_INTERVAL = 60 * 60 * 24
+
+  def twitter_config_cache
+    "#{Dir.tmpdir}/twitter_count_chars_config"
+  end
+
+  def update_twitter_config(force = false)
+    if force ||
+        !File.exist?(twitter_config_cache) || 
+        File.mtime(twitter_config_cache) + CONFIG_UPDATE_INTERVAL + rand(100) <= Time.now
+      client = HTTPClient.new
+      r = client.get('http://api.twitter.com/1/help/configuration.json')
+      if r.status == 200
+        data = JSON.parse(r.body)
+        cache = Hash[data.select{|k,v|["short_url_length", "short_url_length_https"].include?(k)}]
+        File.atomic_write(twitter_config_cache) do |f|
+          f << cache.to_json
+        end
+      else
+        if force || !File.exist?(twitter_config_cache)
+          raise "failed to get twitter config: #{r.inspect}"
+        end
+      end
+    end
+  end
+
+  def twitter_config
+    update_twitter_config
+    open(twitter_config_cache){|f|JSON.parse(f.read)}
+  end
+
+  def short_url_length
+    twitter_config["short_url_length"]
+  end
+
+  def short_url_length_https
+    twitter_config["short_url_length_https"]
+  end
+
+  def length(text)
+    # code from https://dev.twitter.com/docs/counting-characters
+    text = ActiveSupport::Multibyte::Chars.new(text).normalize(:c).to_s
+    # this pattern is not perfect!
+    text = text.gsub(%r{(^|[^0-9A-Za-z_])http(s?)://[^\s/]+(/[\#\$\%\+,\-\./0-9\:;=@A-~\&À-ÿ]*)?}){|s|
+      $1 + 'T' * ($2 == 's' ? short_url_length_https : short_url_length)
+    }
+    ActiveSupport::Multibyte::Chars.new(text).length
+  end
+end
--- spec/twitter_count_chars_spec.rb
+++ spec/twitter_count_chars_spec.rb
@@ -1,7 +1,31 @@
+# -*- coding: utf-8 -*-
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "TwitterCountChars" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  TCC = TwitterCountChars
+  it "should count length" do
+    decomposed ="e\xCC\x81"
+    TCC.length("cafe").should == 4
+    TCC.length("caf#{decomposed}").should == 4
+    TCC.stub!(:short_url_length).and_return(100)
+    TCC.stub!(:short_url_length_https).and_return(101)
+    TCC.length("cafe http://example.com/").should == 105
+    TCC.length("https://example.com/cafécafeかふぇ").should == 104
+    TCC.length("cafehttp://example.com/").should == "cafehttp://example.com/".size
+    TCC.length("http://example.com/").should == 100
+  end
+
+  it "should get short_url_length" do
+    TCC.short_url_length.should >= 19
+    TCC.short_url_length_https.should >= 20
+  end
+
+  it "should update config" do
+    TCC.update_twitter_config(true)
+    open(TCC.twitter_config_cache) do |f|
+      data = JSON.parse(f.read)
+      data["short_url_length"].should >= 19
+      data["short_url_length_https"].should >= 20
+    end
   end
 end
